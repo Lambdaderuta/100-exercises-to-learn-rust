8f74253b68972c6712f35ee5d8f01e9500c9cb8b
ns_lukyanenko <ns_lukyanenko@betboom.org>
2024-06-08 18:34:28 +0300

    Complete 4 chapter

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,6 +4,13 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl<T> From<T> for WrappingU32
+    where  T: PartialOrd + PartialEq{
+    fn from(value: T) -> Self {
+        Self::from(value)
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
